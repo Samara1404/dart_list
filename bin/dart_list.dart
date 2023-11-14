958308a8ceafe33018fe39155a19144b7977ebe5
Samara1404 <stambekova1404@gmail.com>
2023-11-14 23:54:42 +0600

    dart_list

---
+++ bin/dart_list.dart
@@ -0,0 +1,23 @@
+
+
+void main () {
+  var list = ['Samara', 'Maksatbek', 'Nurel', 'Ruslan'];
+  var first = list [0];
+  print(first);
+  list[0] = 'Churok';
+  print(list);
+  void Verona() {  
+
+var list = ['Sam', 'Maks', 'Nur', 'Rus'];
+   print(list.first);
+   print(list.last);
+}
+void main (){
+     
+    var list = ["Tom", "Bob", "Sam"];
+    for (var name in list){
+        print(name);
+    }
+}
+  
+}
